ab6bf205c5b72aeaf9657713084c9e976676177c
jack <jackjack5810@yahoo.com>
2022-02-13 14:10:56 -0500

    update

--- src/surface.ts
+++ src/surface.ts
@@ -1,12 +1,29 @@
 import { InitGPU, CreateGPUBuffer, CreateTransforms, CreateViewProjection, CreateAnimation } from './helper';
-import { Shaders, LightInputs } from './shaders';
+import shader from './shader.wgsl';
 import { vec3, mat4 } from 'gl-matrix';
 const createCamera = require('3d-view-controls');
 
+export interface LightInputs {
+    ambientIntensity?: number;
+    diffuseIntensity?: number;
+    specularIntensity?: number;
+    shininess?: number;
+    specularColor?: vec3;
+    isTwoSideLighting?: number;
+} 
+
 export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normalData: Float32Array, colorData: Float32Array, 
-    lightInputs:LightInputs, isAnimation = true) => {
+    li:LightInputs, isAnimation = true) => {
     const gpu = await InitGPU();
     const device = gpu.device;
+
+    // define default parameters for th elight model
+    li.ambientIntensity = li.ambientIntensity == undefined ? 0.1 : li.ambientIntensity;
+    li.diffuseIntensity = li.diffuseIntensity == undefined ? 0.8 : li.diffuseIntensity;
+    li.specularIntensity = li.specularIntensity == undefined ? 0.4 : li.specularIntensity;
+    li.shininess = li.shininess == undefined ? 30.0 : li.shininess;
+    li.specularColor = li.specularColor == undefined ? [1.0, 1.0, 1.0] : li.specularColor;
+    li.isTwoSideLighting = li.isTwoSideLighting == undefined ? 1 : li.isTwoSideLighting;
 
     // create vertex buffers
     const numberOfVertices = vertexData.length/3;
@@ -14,13 +31,12 @@ export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normal
     const normalBuffer = CreateGPUBuffer(device, normalData);
     const colorBuffer = CreateGPUBuffer(device, colorData);
  
-    const shader = Shaders(lightInputs);
     const pipeline = device.createRenderPipeline({
         vertex: {
             module: device.createShaderModule({                    
-                code: shader.vertex
+                code: shader
             }),
-            entryPoint: "main",
+            entryPoint: "vs_main",
             buffers:[
                 {
                     arrayStride: 12,
@@ -50,9 +66,9 @@ export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normal
         },
         fragment: {
             module: device.createShaderModule({                    
-                code: shader.fragment
+                code: shader
             }),
-            entryPoint: "main",
+            entryPoint: "fs_main",
             targets: [
                 {
                     format: gpu.format as GPUTextureFormat
@@ -83,7 +99,7 @@ export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normal
     let eyePosition = new Float32Array(vp.cameraOption.eye);
     let lightPosition = eyePosition;
 
-    // create uniform buffer and layout
+    // create uniform buffer
     const vertexUniformBuffer = device.createBuffer({
         size: 192,
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
@@ -94,11 +110,22 @@ export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normal
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     });
 
+    var lightParams = [] as any;
+    lightParams.push([li.specularColor[0],li.specularColor[1],li.specularColor[2], 1.0]);
+    lightParams.push([li.ambientIntensity, li.diffuseIntensity, li.specularIntensity, li.shininess]);
+    lightParams.push([li.isTwoSideLighting, 0, 0, 0]);
+
+    const lightUniformBuffer = device.createBuffer({
+        size: 48,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    });
+
     if(isAnimation){
         device.queue.writeBuffer(vertexUniformBuffer, 0, vp.viewProjectionMatrix as ArrayBuffer);
         device.queue.writeBuffer(fragmentUniformBuffer, 0, lightPosition);
         device.queue.writeBuffer(fragmentUniformBuffer, 16, eyePosition);
     }
+    device.queue.writeBuffer(lightUniformBuffer, 0, new Float32Array(lightParams.flat()));
 
     const uniformBindGroup = device.createBindGroup({
         layout: pipeline.getBindGroupLayout(0),
@@ -118,7 +145,15 @@ export const CreateSurfaceWithColormap = async (vertexData: Float32Array, normal
                     offset: 0,
                     size: 32
                 }
-            }                
+            },
+            {
+                binding: 2,
+                resource: {
+                    buffer: lightUniformBuffer,
+                    offset: 0,
+                    size: 48
+                }
+            }                             
         ]
     });
 
